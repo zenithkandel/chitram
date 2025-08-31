7b8306db352a5a6437244a32af5eb332d9110b02
Zenith Kandel <kandelze123@gmail.com>
2025-09-01 04:23:59 +0545

    Enhance artist management view with profile picture upload and display functionality

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Set view engine
 app.set('view engine', 'ejs');
