cf34bc24ad21f92ebfecb83e205ca21e56509823
Zenith Kandel <kandelze123@gmail.com>
2025-09-02 18:01:52 +0545

    Add about page: implement route and create about.ejs view with navigation

--- server.js
+++ server.js
@@ -50,13 +50,9 @@ app.use('/api/applications', applicationRoutes);
 // Root route
 app.get('/', getHomePage);
 
-// Placeholder routes for navigation links
+// About page route
 app.get('/about', (req, res) => {
-    res.send(`
-        <h1>About चित्रम्</h1>
-        <p>About page coming soon...</p>
-        <a href="/">← Back to Home</a>
-    `);
+    res.render('about');
 });
 
 app.get('/apply', (req, res) => {
